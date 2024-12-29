49ac1cee2b6173b2029e03b274ef7eb00eeeb23e
Abdulrahman Al Hariri <abdul.alhariri@ithink365.co.uk>
2024-12-29 06:15:16 +0000

    Added the products display page with search.

---
+++ ThMoCo.WebApp/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ThMoCo.WebApp.Models;
+
+namespace ThMoCo.WebApp.IServices
+{
+    public interface IProductService
+    {
+        Task<List<ProductDTO>> GetProductsAsync(string? search = null, string? category = null, decimal? minPrice = null, decimal? maxPrice = null);
+        Task<ProductDTO> GetProductByIdAsync(int productId);
+        Task<List<ProductDTO>> GetStockStatusAsync();
+        Task UpdateProductCatalogAsync(List<ProductDTO> updatedProducts);
+        Task<List<string>> GetCategoriesAsync();
+    }
+}
