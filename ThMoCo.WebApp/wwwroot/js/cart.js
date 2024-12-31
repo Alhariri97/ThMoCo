b5d2c688deaed07a2d09860da030b320e165c550
Abdulrahman Al Hariri <abdul.alhariri@ithink365.co.uk>
2024-12-31 03:21:58 +0000

    Added cart.

---
+++ ThMoCo.WebApp/wwwroot/js/cart.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $(".add-to-cart-form").on("submit", function (e) {
+        e.preventDefault(); // Prevent the default form submission
+
+        var form = $(this);
+        var actionUrl = form.attr("action");
+        var formData = form.serialize();
+
+        $.ajax({
+            type: "POST",
+            url: actionUrl,
+            data: formData,
+            success: function (response) {
+                // Optional: Update UI, e.g., show a success message
+                alert("Product added to cart!");
+            },
+            error: function () {
+                alert("An error occurred while adding the product to the cart.");
+            }
+        });
+    });
+});
