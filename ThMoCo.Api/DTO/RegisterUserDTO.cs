e56f3e6e4596cf8d268240fa2a1c9bd3cb930f2a
Abdulrahman Al Hariri <abdul.alhariri@ithink365.co.uk>
2025-01-01 05:02:38 +0000

    fixed the dto .

--- ThMoCo.Api/DTO/RegisterUserDTO.cs
+++ ThMoCo.Api/DTO/RegisterUserDTO.cs
@@ -4,21 +4,18 @@
 
 public class RegisterUserDTO
 {
-    public Dictionary<string, object>? app_metadata { get; set; }
-    public DateTime created_at { get; set; }
-    public string email { get; set; }
-    public bool email_verified { get; set; }
-    public string? family_name { get; set; }
-    public string? given_name { get; set; }
-    public DateTime? last_password_reset { get; set; }
-    public string? name { get; set; }
-    public string? nickname { get; set; }
-    public string? phone_number { get; set; }
-    public bool phone_verified { get; set; }
-    public string? picture { get; set; }
-    public string tenant { get; set; }
-    public DateTime updated_at { get; set; }
-    public string user_id { get; set; } // Auth0 User ID
-    public Dictionary<string, object>? user_metadata { get; set; }
-    public string? username { get; set; }
+    public DateTime created_at { get; set; } // User creation time
+    public string email { get; set; } // User email
+    public bool email_verified { get; set; } // Whether email is verified
+    public string? family_name { get; set; } // User's family/last name
+    public string? given_name { get; set; } // User's given/first name
+    public string? name { get; set; } // Full name of the user
+    public string? nickname { get; set; } // User nickname
+    public string? phone_number { get; set; } // User phone number
+    public bool phone_verified { get; set; } // Whether phone is verified
+    public string? picture { get; set; } // URL to user's profile picture
+    public string tenant { get; set; } // Auth0 tenant identifier
+    public DateTime updated_at { get; set; } // Time of last profile update
+    public string user_id { get; set; } // Unique identifier for the user
+    public string? username { get; set; } // Username of the user
 }
