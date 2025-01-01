eac687405b3601e500af8ba7b2930fceb071463f
Abdulrahman Al Hariri <abdul.alhariri@ithink365.co.uk>
2025-01-01 03:51:32 +0000

    Fixed the registeruser dto.

--- ThMoCo.Api/DTO/RegisterUserDTO.cs
+++ ThMoCo.Api/DTO/RegisterUserDTO.cs
@@ -4,21 +4,21 @@
 
 public class RegisterUserDTO
 {
-    public Dictionary<string, object>? AppMetadata { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public string Email { get; set; }
-    public bool EmailVerified { get; set; }
-    public string? FamilyName { get; set; }
-    public string? GivenName { get; set; }
-    public DateTime? LastPasswordReset { get; set; }
-    public string? Name { get; set; }
-    public string? Nickname { get; set; }
-    public string? PhoneNumber { get; set; }
-    public bool PhoneVerified { get; set; }
-    public string? Picture { get; set; }
-    public string Tenant { get; set; }
-    public DateTime UpdatedAt { get; set; }
-    public string UserId { get; set; } // Auth0 User ID
-    public Dictionary<string, object>? UserMetadata { get; set; }
-    public string? Username { get; set; }
+    public Dictionary<string, object>? app_metadata { get; set; }
+    public DateTime created_at { get; set; }
+    public string email { get; set; }
+    public bool email_verified { get; set; }
+    public string? family_name { get; set; }
+    public string? given_name { get; set; }
+    public DateTime? last_password_reset { get; set; }
+    public string? name { get; set; }
+    public string? nickname { get; set; }
+    public string? phone_number { get; set; }
+    public bool phone_verified { get; set; }
+    public string? picture { get; set; }
+    public string tenant { get; set; }
+    public DateTime updated_at { get; set; }
+    public string user_id { get; set; } // Auth0 User ID
+    public Dictionary<string, object>? user_metadata { get; set; }
+    public string? username { get; set; }
 }
