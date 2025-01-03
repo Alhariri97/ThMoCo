4a336b8a59177d685639b72a2b2bfe6d91e238b9
Abdulrahman Al Hariri <abdul.alhariri@ithink365.co.uk>
2025-01-03 14:58:15 +0000

    fixed tets

--- ThMoCo.Api/Program.cs
+++ ThMoCo.Api/Program.cs
@@ -63,48 +63,48 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 
-//// Configure services based on environment
-//if (builder.Environment.IsDevelopment())
-//{
-//    Console.WriteLine("Registering LocalProductService as IProductService");
-//    builder.Services.AddSingleton<IProductService, LocalProductService>();
-//    builder.Services.AddSingleton<IProfileService, LocalProfileService>();
-//    builder.Services.AddSingleton<IOrderService, LocalOrderService>();
-//}
-//else
-//{
-//    //    //Console.WriteLine("Registering ProductService as IProductService");
-//    //    //builder.Services.AddDbContext<ProductsContext>(options =>
-//    //    //{
-//    //    //    var cs = builder.Configuration.GetConnectionString("ConnectionString");
-//    //    //    options.UseSqlServer(cs);
-//    //    //});
-//    //    //builder.Services.AddScoped<IProductService, ProductService>();
-
-
-//    Console.WriteLine("Registering ProductService as IProductService");
-//    builder.Services.AddDbContext<AppDbContext>(options =>
-//    {
-//        var cs = builder.Configuration.GetConnectionString("ConnectionString");
-//        options.UseSqlServer(cs);
-//    });
-//    builder.Services.AddScoped<IProductService, ProductService>();
-//    builder.Services.AddScoped<IProfileService, ProfileService>();
-//    builder.Services.AddScoped<IOrderService, OrderService>();
-//}
-
-////////////////////////////////////
-Console.WriteLine("Registering ProductService as IProductService");
-builder.Services.AddDbContext<AppDbContext>(options =>
+// Configure services based on environment
+if (builder.Environment.IsDevelopment())
 {
-    var cs = builder.Configuration.GetConnectionString("ConnectionString");
-    options.UseSqlServer(cs);
-});
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<IProfileService, ProfileService>();
-builder.Services.AddScoped<IOrderService, OrderService>();
+    Console.WriteLine("Registering LocalProductService as IProductService");
+    builder.Services.AddSingleton<IProductService, LocalProductService>();
+    builder.Services.AddSingleton<IProfileService, LocalProfileService>();
+    builder.Services.AddSingleton<IOrderService, LocalOrderService>();
+}
+else
+{
+    //    //Console.WriteLine("Registering ProductService as IProductService");
+    //    //builder.Services.AddDbContext<ProductsContext>(options =>
+    //    //{
+    //    //    var cs = builder.Configuration.GetConnectionString("ConnectionString");
+    //    //    options.UseSqlServer(cs);
+    //    //});
+    //    //builder.Services.AddScoped<IProductService, ProductService>();
 
-////////////////////////////////////////
+
+    Console.WriteLine("Registering ProductService as IProductService");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        var cs = builder.Configuration.GetConnectionString("ConnectionString");
+        options.UseSqlServer(cs);
+    });
+    builder.Services.AddScoped<IProductService, ProductService>();
+    builder.Services.AddScoped<IProfileService, ProfileService>();
+    builder.Services.AddScoped<IOrderService, OrderService>();
+}
+
+//////////////////////////////////////
+//Console.WriteLine("Registering ProductService as IProductService");
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//{
+//    var cs = builder.Configuration.GetConnectionString("ConnectionString");
+//    options.UseSqlServer(cs);
+//});
+//builder.Services.AddScoped<IProductService, ProductService>();
+//builder.Services.AddScoped<IProfileService, ProfileService>();
+//builder.Services.AddScoped<IOrderService, OrderService>();
+
+//////////////////////////////////////////
 
 
 var app = builder.Build();
